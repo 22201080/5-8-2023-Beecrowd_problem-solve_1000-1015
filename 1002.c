d79b5ea732a50a6cbacb307557093b21293b8c98
22201080 <141312674+22201080@users.noreply.github.com>
2023-08-05 21:09:56 +0600

    Add files via upload

---
+++ 1002.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+       double A,n,R;
+       scanf ("%lf",&R);
+       n=3.1416;
+       A=n*(R*R);
+       printf("A=%.4lf",A);
+       return 0;
+}
---
+++ 1004.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+       int A,B,PORD;
+       scanf("%d%d",A,B);
+       PORD=A+B;
+       printf("PORD = %d",PORD);
+       return 0;
+}
